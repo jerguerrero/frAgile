51338d9b3c9481e435ea21d43cb068b07b8e966d
Judd Guerrero <j.guerrero@student.unimelb.edu.au>
2019-09-01 18:38:06 +1000

    React installation

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const App = () => (
+    <div>
+        <form>
+            <label>
+                Name:
+                <input type="text"/>
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    </div>
+);
+export default App;
