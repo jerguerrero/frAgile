9acccd2bc298ffee35a7c3666a1ce0b4cfa5c14b
Judd Guerrero <j.guerrero@student.unimelb.edu.au>
2019-09-02 22:58:47 +1000

    Routing, Upload Form, Home
    -Added navigation to Homepage, and Upload Form
    -Worked out the dynamic part of the upload form

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
-const App = () => (
-    <div>
-        <form>
-            <label>
-                Name:
-                <input type="text"/>
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
-    </div>
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import  Upload  from '../Upload';
+import Home from '../Home';
+const App = (props) => {
+
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/Upload">Upload</Link>
+                    </li>
+                </ul>
+                <hr />
+
+                <Route exact path="/" component={Home} />
+                <Route exact path="/Upload" component={Upload} />
+            </div>
+        </Router>
+
+
+
 );
+}
+
+App.propTypes = {
+    name: PropTypes.string.isRequired
+};
+
 export default App;
