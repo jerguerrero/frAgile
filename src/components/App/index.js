61cf04c1cdedc83e41afb832c219e8458d9d3de3
unknown <iyuwono@student.unimelb.edu.au>
2019-09-04 22:42:04 +1000

    Login page front end

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import  Upload  from '../Upload';
 import Home from '../Home';
+import Login from '../Login';
 const App = (props) => {
 
     return (
@@ -15,11 +16,15 @@ const App = (props) => {
                     <li>
                         <Link to="/Upload">Upload</Link>
                     </li>
+                    <li>
+                        <Link to="/Login">Login</Link>
+                    </li>
                 </ul>
                 <hr />
 
                 <Route exact path="/" component={Home} />
                 <Route exact path="/Upload" component={Upload} />
+                <Route exact path="/Login" component={Login} />
             </div>
         </Router>
 
