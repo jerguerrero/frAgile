4071dff15d8696ebaca00f60e7fc64554a76dc43
Judd Guerrero <j.guerrero@student.unimelb.edu.au>
2019-09-05 13:07:17 +1000

    Grids, List

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,8 +4,23 @@ import firebase from '../Firebase/index.js';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import Img from 'react-image';
 import '../../stylesheet.css'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
 
 const Home = () => {
+
+    const useStyles = makeStyles(theme => ({
+        root: {
+            width: '100%',
+            maxWidth: 360,
+            backgroundColor: "white",
+        },
+    }));
+
+    const classes = useStyles();
 
     const db = firebase.firestore();
 
@@ -23,23 +38,50 @@ const Home = () => {
     }
 
     return (
-    <div>
-        <Infinite containerHeight={200} elementHeight={40}>
-        {(() => {
-            if(value){
-                return value.docs.map(document => {
-                    console.log(document);
-                    return(<li
-                        onClick={()=>{setCurrentImage(document.data().imageUrl)}}>
-                        {JSON.stringify(document.data().Name)}
-                    </li>);
-                });
-            }
-        })()}
-        </Infinite>
-        <Img src={currentImage}/>
-        <h1>HomePage</h1>
-    </div>
+<div>
+    <h1>Artifacts</h1>
+        <Grid container direction="row"
+              justify="space-between"
+              alignItems="center"
+        spacing={100}>
+
+            {/*<div >*/}
+           
+
+            <Grid item xs={3} alignItems="center">
+                <Img  style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }} src={currentImage}/>
+            </Grid>
+
+            <Grid item justify="flex-end" xs={3}>
+                <List className={classes.root}>
+                {/*<Infinite*/}
+                    {/*containerHeight={200}*/}
+                    {/*elementHeight={40}>*/}
+                {(() => {
+                    if(value){
+                        return value.docs.map(document => {
+                            console.log(document);
+                            return(
+
+                                <ListItem button
+                                onClick={()=>{setCurrentImage(document.data().imageUrl)}}>
+                                    <ListItemText primary={JSON.stringify(document.data().Name)} />
+
+                            </ListItem>);
+                        });
+                    }
+                })()}
+                {/*</Infinite>*/}
+                </List>
+            </Grid>
+
+
+            {/*</div>*/}
+        </Grid>
+</div>
     );
 }
 export default Home;
