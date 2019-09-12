2658277ba8e7f8a8a8261ab81672b9cd4717afef
Judd Guerrero <j.guerrero@student.unimelb.edu.au>
2019-09-12 10:12:31 +1000

    Color Schemes, Size of Divs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,29 +3,59 @@ import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import  Upload  from '../Upload';
 import Home from '../Home';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import './app.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faFileUpload } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome, faFileUpload )
+
 const App = (props) => {
 
-    return (
-        <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/Upload">Upload</Link>
-                    </li>
-                </ul>
-                <hr />
 
+    return (
+
+        <Router>
+            <div >
+
+                <div id={"mainappcontainer"}>
+
+                </div>
+                <AppBar id={"mainappbar"} position="static" >
+
+                    <Grid container
+                          direction="row"
+                          justify="flex-start"
+                          alignItems="center"
+                          spacing={2}>
+                        <Grid item>
+                            <Link to="/" class={"button"}>
+                                <FontAwesomeIcon
+                                    icon="home"
+                                    size="xl"
+                                />
+                                {" Home"}
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link to="/Upload" class={"button"}>
+                                <FontAwesomeIcon
+                                    icon="file-upload"
+                                    size="xl"
+                                />
+                                {" Upload"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </AppBar>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/Upload" component={Upload} />
             </div>
         </Router>
-
-
-
-);
-}
+    );
+};
 
 export default App;
