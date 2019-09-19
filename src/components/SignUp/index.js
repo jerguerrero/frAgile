cbae2e6dfd9ad3754bae4d631e5d6740890c73b7
Judd Guerrero <jguerrero@student.unimelb.edu.au>
2019-09-19 10:38:27 +1000

    changing login to functional formal

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,14 +1,80 @@
 import React, {Component, useState} from 'react';
 import { Link } from 'react-router-dom';
 import firebase from '../Firebase';
+import {useAlert} from "react-alert";
 
-const SignUpPage = () => (
+const SignUpPage = (props) => {
 
-    <div>
-        <h1>SignUp</h1>
-        <SignUpForm />
-    </div>
-);
+    const db = firebase.firestore();
+    const auth = firebase.auth();
+    const alert = useAlert();
+
+    // State for for form values
+    const [formValues, setFormValues] = useState({});
+    // State for extra input fields
+    const [fields, setFields] = useState(["Username", "Email", "Password", "Confirm Password"]);
+
+    const handleInputChange = (event) => {
+        //Adds new value
+        setFormValues({...formValues, [event.target.name]: event.target.value});
+    };
+
+    const registerUser = (event) => {
+        var user = formValues;
+        auth.createUserWithEmailAndPassword(user.email, user.password)
+            .then(authUser => {
+                this.setState({...INITIAL_STATE});
+            })
+            .catch(error => {
+                this.setState({error});
+            });
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <h1>SignUp</h1>
+            <form onSubmit={this.onSubmit}>
+                <input
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Full Name"
+                />
+                <br/>
+                <br/>
+                <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <br/>
+                <br/>
+                <input
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <br/>
+                <br/>
+                <input
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                />
+                <button disabled={isInvalid} type="submit">Sign Up</button>
+                {error && <p>{error.message}</p>}
+            </form>
+        </div>
+    )
+};
 
 const INITIAL_STATE = {
     username: '',
@@ -24,6 +90,7 @@ class SignUpForm extends Component {
 
         this.state = { ...INITIAL_STATE };
     }
+
 
     onSubmit = event => {
         const { username, email, passwordOne } = this.state;
