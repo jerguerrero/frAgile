d3e98ed81324d14172d64ae0af9b87003ae3172a
unknown <iyuwono@student.unimelb.edu.au>
2019-09-22 15:04:07 +1000

    Added a log out button on the navigation bar but still not working yet

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,6 +14,7 @@ import Login from '../Login';
 
 // for testing only, should be deleted in the future
 import SignUp from '../SignUp';
+import LogOutButton from '../LogOut';
 
 library.add(faHome, faFileUpload );
 
@@ -61,6 +62,10 @@ const App = (props) => {
                         {/*for testing only, should be deleted in the future*/}
                         <Grid item>
                             <Link to="/SignUp">SignUp</Link>
+                        </Grid>
+                        {/*for testing only, should be deleted in the future*/}
+                        <Grid item>
+                            <LogOutButton/>
                         </Grid>
 
                     </Grid>
